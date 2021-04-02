bc74b7c18aaf28a6280a38311b4702b14ee84b96
keerthi <keerthireddy932@gmail.com>
2021-04-02 15:31:08 +0530

    adding files

---
+++ MyrecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myrecyclerview;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView lv = findViewById(R.id.rv);
+        String[] s = {"s"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,s);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, ""+parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ Recyclerviewproject/app/src/main/java/com/example/recyclerviewproject/Myadapter.java
@@ -0,0 +1,4 @@
+package com.example.recyclerviewproject;
+
+public class Myadapter {
+}
