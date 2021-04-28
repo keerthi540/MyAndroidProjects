151966166bb79dec4550a883ab7bd4ed917a7a81
keerthi <keerthireddy932@gmail.com>
2021-04-28 07:03:15 +0530

    project

---
+++ ProHelpR/app/src/main/java/com/example/prohelpr/workers/WorkerHome.java
@@ -0,0 +1,16 @@
+package com.example.prohelpr.workers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.prohelpr.R;
+
+public class WorkerHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worker_home);
+    }
+}
